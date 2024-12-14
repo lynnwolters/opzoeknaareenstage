12cabe97ee4118ce86f20fe4f8d72999b6504395
Lynn Wolters <lynn.wolters27@gmail.com>
2024-12-14 21:56:50 +0100

    New

--- js/overlay.js
+++ js/overlay.js
@@ -1,27 +1,27 @@
-const overlayButton = document.querySelector("#overlay button");
-let overlayDismissed = false; 
+// const overlayButton = document.querySelector("#overlay button");
+// let overlayDismissed = false; 
 
-function checkScreenWidth() {
-    const overlay = document.querySelector("#overlay");
-    if (window.innerWidth < 900 && !overlayDismissed) {
-        overlay.classList.add("overlay-active");
-    } else if (window.innerWidth >= 900 || overlayDismissed) {
-        overlay.classList.remove("overlay-active");
-    }
-}
+// function checkScreenWidth() {
+//     const overlay = document.querySelector("#overlay");
+//     if (window.innerWidth < 900 && !overlayDismissed) {
+//         overlay.classList.add("overlay-active");
+//     } else if (window.innerWidth >= 900 || overlayDismissed) {
+//         overlay.classList.remove("overlay-active");
+//     }
+// }
 
-function setupOverlayButton() {
-    const overlay = document.querySelector("#overlay");
-    const overlayButton = document.querySelector("#overlay button");
-    overlayButton.addEventListener("click", () => {
-        overlay.classList.remove("overlay-active");
-        overlayDismissed = true; 
-    });
-}
+// function setupOverlayButton() {
+//     const overlay = document.querySelector("#overlay");
+//     const overlayButton = document.querySelector("#overlay button");
+//     overlayButton.addEventListener("click", () => {
+//         overlay.classList.remove("overlay-active");
+//         overlayDismissed = true; 
+//     });
+// }
 
-window.addEventListener("load", () => {
-    checkScreenWidth();
-    setupOverlayButton();
-});
-window.addEventListener("resize", checkScreenWidth);
+// window.addEventListener("load", () => {
+//     checkScreenWidth();
+//     setupOverlayButton();
+// });
+// window.addEventListener("resize", checkScreenWidth);
 
