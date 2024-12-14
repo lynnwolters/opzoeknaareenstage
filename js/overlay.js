c4959af0837d9dc83de60fc0b645f6fd5a85bedf
Lynn Wolters <lynn.wolters27@gmail.com>
2024-12-14 21:19:14 +0100

    New

--- js/overlay.js
+++ js/overlay.js
@@ -1,24 +1,30 @@
-const overlayButton = document.querySelector("#overlay button");
+window.addEventListener("load", () => {
+    const overlayButton = document.querySelector("#overlay button");
+    const overlay = document.querySelector("#overlay");
 
-function checkScreenWidth() {
+    function checkScreenWidth() {
+        if (window.innerWidth < 900) {
+            overlay.classList.add("overlay-active");
+        } else {
+            overlay.classList.remove("overlay-active");
+        }
+    }
+
+    function setupOverlayButton() {
+        overlayButton.addEventListener("click", () => {
+            overlay.classList.remove("overlay-active");
+        });
+    }
+
+    checkScreenWidth();
+    setupOverlayButton();
+});
+
+window.addEventListener("resize", () => {
     const overlay = document.querySelector("#overlay");
     if (window.innerWidth < 900) {
         overlay.classList.add("overlay-active");
     } else {
         overlay.classList.remove("overlay-active");
     }
-}
-
-function setupOverlayButton() {
-    const overlay = document.querySelector("#overlay");
-    overlayButton.addEventListener("click", () => {
-        overlay.classList.remove("overlay-active");
-    });
-}
-
-// Zorg dat beide functies correct worden aangeroepen
-window.addEventListener("load", () => {
-    checkScreenWidth();
-    setupOverlayButton();
 });
-window.addEventListener("resize", checkScreenWidth);
