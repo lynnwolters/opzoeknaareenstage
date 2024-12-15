1de0bf0352e1d7fee932facc8cea6e9ea752eab3
Lynn Wolters <lynn.wolters27@gmail.com>
2024-12-15 19:00:20 +0100

    New

--- js/contentFadeIn.js
+++ js/contentFadeIn.js
@@ -1,23 +1,24 @@
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-// gsap.utils.toArray(".content-fade-in").forEach((element) => {
-//     gsap.fromTo(
-//         element,
-//         {
-//             opacity: 0,
-//             y: 20, 
-//         },
-//         {
-//             opacity: 1,
-//             y: 0, 
-//             duration: 1, 
-//             ease: "power2.out",
-//             scrollTrigger: {
-//             trigger: element,
-//             start: "-200px 80%", 
-//             toggleActions: "play none none reset", 
-//             markers: true,
-//             },
-//         }
-//     );
-// });
+gsap.utils.toArray(".content-fade-in").forEach((element) => {
+    const containerContentFadeIn = element.closest(".container-content-fade-in"); 
+
+    gsap.fromTo(
+        element,
+        {
+            opacity: 0,
+            y: 200, 
+        },
+        {
+            opacity: 1,
+            y: 0, 
+            duration: 1, 
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: containerContentFadeIn, 
+                start: "top 80%", 
+                toggleActions: "play none none reverse",
+            },
+        }
+    );
+});
